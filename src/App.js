973c6a97151ff93eb26098e145b44133c0c012fb
Naheen Habib <naheenhabib@outlook.com>
2022-10-26 21:56:56 +1100

    new commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <h1>Lets build gmail</h1>
     </div>
   );
