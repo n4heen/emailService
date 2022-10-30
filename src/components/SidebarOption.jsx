720d9dd5ee1079697be5dad848d62539ab6e3b93
Naheen Habib <naheenhabib@outlook.com>
2022-10-30 14:24:17 +1100

    new commit

--- src/components/SidebarOption.jsx
+++ src/components/SidebarOption.jsx
@@ -3,7 +3,7 @@ import "./SidebarOption.css";
 const SidebarOption = ({Icon,title,number}) => {
     return (
         <div className='sidebarOption'>
-            <Icon/>
+           {Icon}
             <h3>{title}</h3>
             <p>{number}</p>
         </div>
