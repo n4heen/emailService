a521d44cde609d9487dd57f596b7944760738699
Naheen Habib <naheenhabib@outlook.com>
2022-10-30 14:11:37 +1100

    new commit

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import { Button } from "@mui/material";
+import React from "react";
 import "./Sidebar.css";
+import AddIcon from "@mui/icons-material/Add";
+import InboxIcon from '@mui/icons-material/Inbox';
+import SidebarOption from "./SidebarOption";
 const Sidebar = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-}
+  return (
+    <div className="sidebar">
+      <Button
+        startIcon={<AddIcon fontSize="large" />}
+        className="sidebarCompose"
+      >
+        Compose
+      </Button>
+      <SidebarOption Icon={<InboxIcon/>} title="Inbox" number={64}/>
+    </div>
+  );
+};
 
 export default Sidebar;
