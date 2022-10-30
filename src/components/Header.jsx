ef3d3bc8bfc6f7870bf702529883d4eee93d7eee
Naheen Habib <naheenhabib@outlook.com>
2022-10-30 13:27:45 +1100

    new commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,42 @@
-import React from 'react';
-
+import React from "react";
+import "./Header.css";
+import { IconButton } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import AppsIcon from "@mui/icons-material/Apps";
+import SearchIcon from "@mui/icons-material/Search";
+import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import { ArrowDropDown } from "@mui/icons-material";
 const Header = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-}
+  return (
+    <div className="header">
+      <div className="headerLeft">
+        <IconButton>
+          <MenuIcon />
+        </IconButton>
+        <img
+          src="https://i.pinimg.com/originals/ae/47/fa/ae47fa9a8fd263aa364018517020552d.png"
+          alt=""
+        />
+      </div>
+      <div className="headerMiddle">
+        <SearchIcon />
+        <input type="text" placeholder="Search mail" />
+        <ArrowDropDown className="header__inputCaret" />
+      </div>
+      <div className="headerRight">
+        <IconButton>
+          <HelpOutlineIcon />
+        </IconButton>
+        <IconButton>
+          <SettingsOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <AppsIcon />
+        </IconButton>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
