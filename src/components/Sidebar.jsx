a9f846dc4dfd9c86018e02788393add27ff7457b
Naheen Habib <naheenhabib@outlook.com>
2022-10-30 14:43:00 +1100

    new commit

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,18 @@
-import { Button } from "@mui/material";
+import { Button, IconButton } from "@mui/material";
 import React from "react";
 import "./Sidebar.css";
 import AddIcon from "@mui/icons-material/Add";
-import InboxIcon from '@mui/icons-material/Inbox';
+import InboxIcon from "@mui/icons-material/Inbox";
 import SidebarOption from "./SidebarOption";
+import StarIcon from "@mui/icons-material/Star";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import LabelImportantIcon from "@mui/icons-material/LabelImportant";
+import NearMeIcon from "@mui/icons-material/NearMe";
+import NoteIcon from "@mui/icons-material/Note";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import PersonIcon from "@mui/icons-material/Person";
+import DuoIcon from "@mui/icons-material/Duo";
+import PhoneIcon from "@mui/icons-material/Phone";
 const Sidebar = () => {
   return (
     <div className="sidebar">
@@ -13,7 +22,35 @@ const Sidebar = () => {
       >
         Compose
       </Button>
-      <SidebarOption Icon={<InboxIcon/>} title="Inbox" number={64}/>
+      <SidebarOption
+        Icon={<InboxIcon />}
+        title="Inbox"
+        number={64}
+        selected={true}
+      />
+      <SidebarOption Icon={<StarIcon />} title="Starred" number={64} />
+      <SidebarOption Icon={<AccessTimeIcon />} title="Snoozed" number={64} />
+      <SidebarOption
+        Icon={<LabelImportantIcon />}
+        title="Important"
+        number={64}
+      />
+      <SidebarOption Icon={<NearMeIcon />} title="Sent" number={64} />
+      <SidebarOption Icon={<NoteIcon />} title="Drafts" number={64} />
+      <SidebarOption Icon={<ExpandMoreIcon />} title="More" number={64} />
+      <div className="sidebarFooter">
+        <div className="sidebarFooterIcons">
+          <IconButton>
+            <PersonIcon />
+          </IconButton>
+          <IconButton>
+            <DuoIcon />
+          </IconButton>
+          <IconButton>
+            <PhoneIcon />
+          </IconButton>
+        </div>
+      </div>
     </div>
   );
 };
